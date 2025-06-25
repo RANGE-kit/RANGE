1999db9f52fb315eb2027c19b5b8a64e6093d645
Zhang <d2j@ornl.gov>
2025-06-25 10:41:10 -0400

    Setup installation

---
+++ setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 25 10:27:07 2025
+
+@author: d2j
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="hagois",
+    version="0.1",
+    author="d2j",
+    author_email="Email Here",
+    description="Hybrid ABC-GA for Optimizing Interactive Systems",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/zhangdf07/GA_ABC",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        # Add your dependencies here, e.g.
+        # "numpy>=1.20",
+        # "scipy>=1.5",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Materials Science",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
